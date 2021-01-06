4e4c63d0cbd5735d37d9e03e93c22d35cdba6448
Björn Arnelid <bjorn.arnelid@gmail.com>
2021-01-06 11:46:52 +0100

    Day 15 pt 2
    Solved using dict

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,17 +1,20 @@
 def number_game(starting_numbers, iterations):
-    number_series = starting_numbers.copy()
+    number_map = {}
+    for position, number in enumerate(starting_numbers[:-1]):
+        number_map[number] = position
+    last_number = starting_numbers[-1]
     for i in range(iterations):
-        last_number = number_series[-1]
-        try:
-            numbers_to_remember = number_series[:-1]
-            index = numbers_to_remember[::-1].index(last_number)
-            age = index + 1
-            number_series.append(age)
-        except ValueError:
-            number_series.append(0)
-    return number_series
+        last_position = (i + len(starting_numbers) - 1)
+        remembered_position = number_map.get(last_number)
+        number_map[last_number] = last_position
+        if remembered_position is not None:
+            last_number = last_position - remembered_position
+        else:
+            last_number = 0
+    return last_number
 
 
 if __name__ == '__main__':
-    print('2020th number is: ' + str(number_game([0, 20, 7, 16, 1, 18, 15], 2020)[2019]))
-    print('30000000th number is: ' + str(number_game([0, 20, 7, 16, 1, 18, 15], 30000000)[29999999]))
+    input_numbers = [0, 20, 7, 16, 1, 18, 15]
+    print('2020th number is: ' + str(number_game(input_numbers, 2020 - len(input_numbers))))
+    print('30000000th number is: ' + str(number_game([0, 20, 7, 16, 1, 18, 15], 30000000- len(input_numbers))))
--- 2020/testday15.py
+++ 2020/testday15.py
@@ -5,22 +5,34 @@ from day15 import number_game
 
 class Day15Test(unittest.TestCase):
     def test_iterate_once(self):
-        self.assertEqual(0, number_game([0, 3, 6], 1)[-1])
+        self.assertEqual(0, number_game([0, 3, 6], 1))
 
     def test_iter_twice(self):
-        self.assertEqual(3, number_game([0, 3, 6], 2)[-1])
+        self.assertEqual(3, number_game([0, 3, 6], 2))
 
     def test_iter_thrice(self):
-        self.assertEqual(3, number_game([0, 3, 6], 3)[-1])
+        self.assertEqual(3, number_game([0, 3, 6], 3))
 
     def test_iter_fourth(self):
-        self.assertEqual(1, number_game([0, 3, 6], 4)[-1])
+        self.assertEqual(1, number_game([0, 3, 6], 4))
+
+    def test_iter_fifth(self):
+        self.assertEqual(0, number_game([0, 3, 6], 5))
+
+    def test_iter_sixth(self):
+        self.assertEqual(4, number_game([0, 3, 6], 6))
+
+    def test_iter_seventh(self):
+        self.assertEqual(0, number_game([0, 3, 6], 7))
 
     def test_2020_iterations(self):
-        self.assertEqual(1, number_game([1, 3, 2], 2020)[2019])
+        self.assertEqual(436, number_game([0, 3, 6], 2017))
 
-    def test_2020_iterations(self):
-        self.assertEqual(10, number_game([2, 1, 3], 2020)[2019])
+    def test_2020_iterations2(self):
+        self.assertEqual(1, number_game([1, 3, 2], 2017))
+
+    def test_2020_iterations3(self):
+        self.assertEqual(10, number_game([2, 1, 3], 2017))
 
 
 if __name__ == '__main__':
