2ac19b0a075ec4441fdfa82dcd7b7a37555aa28f
Björn Arnelid <bjorn.arnelid@xylem.com>
2023-12-02 13:39:00 +0100

    Day 2, 2nd star

--- python/2023/day2.py
+++ python/2023/day2.py
@@ -1,27 +1,56 @@
-def is_invalid(content):
-    for cube_set in content.split(","):
-        [amount, color] = cube_set.strip().split(" ")
-        if color == "red" and int(amount) > 12:
-            return True
-        elif color == "green" and int(amount) > 13:
-            return True
-        elif color == "blue" and int(amount) > 14:
-            return True
-    return False
+class CubeBag:
+    def __init__(self):
+        self.red = 0
+        self.blue = 0
+        self.green = 0
+
+    def parse_hand(self, hand):
+        for cube_set in hand.split(","):
+            [amount_string, color] = cube_set.strip().split(" ")
+            amount = int(amount_string)
+            if color == "red":
+                self.red = max(self.red, amount)
+            elif color == "green":
+                self.green = max(self.green, amount)
+            elif color == "blue":
+                self.blue = max(self.blue, amount)
+
+
+def count_bag(content):
+    bag = CubeBag()
+    for pull in content.split(";"):
+        bag.parse_hand(pull)
+    return bag
+
+
+def is_invalid(bag):
+    return bag.red > 12 or bag.green > 13 or bag.blue > 14
 
 
 def verify(string):
     [game_id, content] = string.split(":")
-    for pull in content.split(";"):
-        if is_invalid(pull):
-            return 0
+    bag = count_bag(content)
+    if is_invalid(bag):
+        return 0
     return int(game_id.split(" ")[1])
+
+
+def count(string):
+    [_, content] = string.split(":")
+    bag = count_bag(content)
+    return bag.green * bag.red * bag.blue
 
 
 if __name__ == '__main__':
     print("Starting day 2")
-    total = 0
     with open('data/day2.data') as data:
-        for line in data:
-            total += verify(line)
-        print("Answer part 1: " + str(total))
+        data_list = list(data)
+    total = 0
+    for line in data_list:
+        total += verify(line)
+    print("Answer part 1: " + str(total))
+
+    total = 0
+    for line in data_list:
+        total += count(line)
+    print("Answer part 2: " + str(total))
--- python/2023/testday2.py
+++ python/2023/testday2.py
@@ -1,27 +1,49 @@
 import unittest
 
-from day2 import verify
+from day2 import verify, count, CubeBag
 
 
 class MyTestCase(unittest.TestCase):
-    def test_string_1(self):
+    def test_verify_1(self):
         self.assertEqual(1, verify("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
 
-    def test_string_2(self):
+    def test_verify_2(self):
         self.assertEqual(2, verify("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"))
 
-    def test_string_3(self):
+    def test_verify_3(self):
         self.assertEqual(0, verify("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"))
 
-    def test_string_4(self):
+    def test_verify_4(self):
         self.assertEqual(0, verify("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"))
 
-    def test_string_5(self):
+    def test_verify_5(self):
         self.assertEqual(5, verify("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"))
 
-    def test_string_8(self):
-        self.assertEqual(0, verify("Game 99: 1 green, 11 red, 12 blue; 7 red, 20 blue, 1 green; 5 blue, 5 red; 6 blue, 4 red; 1 blue, 1 green; 6 red, 8 blue"))
+    def test_verify_8(self):
+        self.assertEqual(0, verify("Game 99: 1 green, 11 red, 12 blue; 7 red, 20 blue, 1 green; 5 blue, 5 red; 6 "
+                                   "blue, 4 red; 1 blue, 1 green; 6 red, 8 blue"))
 
+    def test_parse_hand_1(self):
+        test_hand = CubeBag()
+        test_hand.parse_hand("3 blue, 4 red")
+        self.assertEqual(3, test_hand.blue)
+        self.assertEqual(4, test_hand.red)
+        self.assertEqual(0, test_hand.green)
+
+    def test_count_1(self):
+        self.assertEqual(48, count("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"))
+
+    def test_count_2(self):
+        self.assertEqual(12, count("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"))
+
+    def test_count_3(self):
+        self.assertEqual(1560, count("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"))
+
+    def test_count_4(self):
+        self.assertEqual(630, count("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"))
+
+    def test_count_5(self):
+        self.assertEqual(36, count("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"))
 
 if __name__ == '__main__':
     unittest.main()
